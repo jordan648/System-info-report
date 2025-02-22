2da86f6dc46b30d9ce29bfbdbf1b989e07297b4a
jordan648 <jordanbkrause@icloud.com>
2025-02-22 10:05:52 -0600

    Added the ability to save the report to a csv file with the current date and time in the file name.

--- system-info-report.ps1
+++ system-info-report.ps1
@@ -1,7 +1,21 @@
-Get-CimInstance Win32_Processor | Select-Object Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed, Manufacturer, SocketDesignation, L2CacheSize, L3CacheSize, Architecture, DataWidth, AddressWidth, Family, ProcessorId, Revision, Status, Version
-Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, BuildNumber, OSArchitecture, LastBootUpTime, InstallDate, SerialNumber, SystemDrive, WindowsDirectory, Organization, RegisteredUser, NumberOfUsers, NumberOfProcesses, NumberOfLicensedUsers, NumberOfProcesses, NumberOf
-Get-CimInstance Win32_PhysicalMemory
-Get-PSDrive -PSProvider FileSystem | Select-Object Name, Used, Free, Provider, Root, Description
-Get-NetIPAddress
-Get-Process | Select-Object Name, Id, CPU, Path
-Out-File -FilePath system-info-report.csv -Encoding utf8
+$filename = 'system-info-report' + (Get-Date -Format 'yyyyMMdd-HHmmss') + '.csv'
+
+"System Report" + (Get-Date -Format 'yyyy-MM-dd HH:mm:ss') | Out-File -FilePath $filename -Encoding utf8
+
+"CPU Info:" | Out-File -FilePath $filename -Encoding utf8
+Get-CimInstance Win32_Processor | Select-Object Name, NumberOfCores, NumberOfLogicalProcessors, MaxClockSpeed, Manufacturer, Architecture, Family, ProcessorId, Status, Version | Out-File -FilePath $filename -Append -Encoding utf8
+
+"OS Info:" | Out-File -FilePath $filename -Append -Encoding utf8
+Get-CimInstance Win32_OperatingSystem | Select-Object Caption, Version, BuildNumber, OSArchitecture, LastBootUpTime, InstallDate, SystemDrive, WindowsDirectory, Organization, NumberOfUsers, NumberOfProcesses | Out-File -FilePath $filename -Append -Encoding utf8
+
+"Memory Info:" | Out-File -FilePath $filename -Append -Encoding utf8
+Get-CimInstance Win32_PhysicalMemory | Out-File -FilePath $filename -Append -Encoding utf8
+
+"Disk Info:" | Out-File -FilePath $filename -Append -Encoding utf8
+Get-PSDrive -PSProvider FileSystem | Select-Object Name, Used, Free, Provider, Root, Description | Out-File -FilePath $filename -Append -Encoding utf8
+
+"Network Info:" | Out-File -FilePath $filename -Append -Encoding utf8
+Get-NetIPAddress | Out-File -FilePath $filename -Append -Encoding utf8
+
+"Process Info:" | Out-File -FilePath $filename -Append -Encoding utf8
+Get-Process | Select-Object Name, Id, CPU, Path | Out-File -FilePath $filename -Append -Encoding utf8
